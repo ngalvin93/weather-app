d1391ec4150538a5aade2899081957391cc34f4b
Alvin Ng <ngalvin93@gmail.com>
2020-02-22 23:19:01 -0600

    get location

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -10,30 +10,36 @@ export class Temperature extends Component {
     }
 
     componentDidMount() {
-        function getGeolocation () {
-            return new Promise(function (resolve, reject) {
-                navigator.geolocation.getCurrentPosition((geolocation) => {
-                    this.setState({ latitude: geolocation.coords.latitude, longitude: geolocation.coords.longitude})
-                    resolve([geolocation.coords.latitude, geolocation.coords.longitude])
-                })
+        // function getGeolocation () {
+        //     return new Promise(function (resolve, reject) {
+        //         navigator.geolocation.getCurrentPosition((geolocation) => {
+        //             // this.setState({ latitude: geolocation.coords.latitude, longitude: geolocation.coords.longitude})
+        //             resolve([geolocation.coords.latitude, geolocation.coords.longitude])
+        //         })
+        //     })
+        // }
+
+        // function fetchWeather (geolocation) {
+        //     console.log('fetch weather', geolocation)
+        //     return new Promise(function (resolve, reject) {
+        //         axios.get(`https://fcc-weather-api.glitch.me/api/current?lat=${Math.floor(this.state.latitude)}&lon=${Math.floor(this.state.longitude)}`)
+        //             .then(res => {
+        //                 console.log('response',res)
+        //                 resolve(res)
+        //             })
+        //     })
+        // }
+
+        // getGeolocation()
+        //     .then(fetchWeather)
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((location)=> {
+                this.setState({latitude: location.coords.latitude, longitude: location.coords.longitude})
+                console.log('the location', this.state.latitude, this.state.longitude)
             })
+        } else {
+            console.log('Geolocation not found')
         }
-
-        function fetchWeather (geolocation) {
-            console.log('fetch weather', geolocation)
-            return new Promise(function (resolve, reject) {
-                axios.get(`https://fcc-weather-api.glitch.me/api/current?lat=${Math.floor(this.state.latitude)}&lon=${Math.floor(this.state.longitude)}`)
-                    .then(res => resolve(res))
-            })
-        }
-
-        getGeolocation()
-            .then(fetchWeather)
-
-        // navigator.geolocation.getCurrentPosition((location)=> {
-        //     this.setState({location: location.coords})
-        //     console.log('the location', this.state.location)
-        // })
         // axios.get('https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139')
         //     .then(res => {
         //         this.setState({data: res})
