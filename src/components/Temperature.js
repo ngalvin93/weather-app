880073e3d4be74ef644afa6c7fb277f0a72a3850
Alvin Ng <ngalvin93@gmail.com>
2020-02-26 23:22:36 -0600

    unit switch

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -17,13 +17,9 @@ export class Temperature extends Component {
             const longitude = Math.floor(location.coords.longitude)
             axios.get(`https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`)
                 .then(res => {
-                    console.log('fetched', res.data)
-                    let cTemp = res.data.main.temp
-                    console.log(cTemp, 'C')
-                    let fTemp = cTemp * 9 / 5 + 32
-                    console.log(fTemp, 'F')
+                    console.log('fetched data', res.data)
                     this.setState({temp: res.data.main.temp})
-                    console.log('temp',this.state.temp)
+                    console.log('state temp',this.state.temp)
                     this.setState({ loading: false })
                 })
         })
@@ -32,14 +28,16 @@ export class Temperature extends Component {
     switchUnit = (e) => {
         console.log('clicked', e.target.innerHTML)
         this.setState({ unit: !this.state.unit })
-        e.target.innerHTML = (this.state.unit ? e.target.innerHTML * 9 / 5 + 32 : e.target.innerHTML)
+        //e.target.innerHTML = this.state.unit ? e.target.innerHTML : (9 * e.target.innerHTML + (32 * 5)) / 5
+        this.setState({ temp: this.state.unit ? (this.state.temp - 32) * 5 / 9 : this.state.temp * 9 / 5 + 32})
         console.log('UNIT',this.state.unit)
+        console.log('temp',this.state.temp)
     }
 
     render() {
         return (
             <div id='unit-btn'>
-                <p onClick={ this.switchUnit }>{this.state.loading ? 'WAITING' : this.state.temp}</p>
+                <p onClick={ this.switchUnit }>{ this.state.loading ? 'WAITING' : this.state.temp }</p>
             </div>
         )
     }
